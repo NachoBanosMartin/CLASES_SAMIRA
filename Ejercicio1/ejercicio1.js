cfa74f2e8a2d98727c4da644f03aff3195c3989b
NachoBanosMartin <ibannosm01@educarex.es>
2025-09-21 11:51:12 +0200

    Subo

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,7 @@
+let precio = 200;
+let iva = 21;
+
+let ivaProducto = (precio * iva) / 100;
+let precioFinal = precio + ivaProducto;
+
+console.log("Precio final del producto:",precioFinal, "€");
---
+++ Ejercicio2/ejercicio2.js
@@ -0,0 +1,8 @@
+let lado = 40;
+
+let area = lado * lado;
+let perimetro = lado * 4;
+
+console.log("El área del cuadrado es de:", area);
+alert("El perímetro del cuadrado es de: " + perimetro);
+
